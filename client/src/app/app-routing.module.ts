5d677c3a9a404728439abfd63128c7a65f79bc74
ZakariaZair <zakaria.zair@polymtl.ca>
2023-04-06 22:28:28 -0400

    added own component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: AppComponent },
+  { path: 'map', component: MainPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
