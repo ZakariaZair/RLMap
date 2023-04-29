af0b76067d20508318a67f699aadb64107dbc3f3
ZakariaZair <89856615+ZakariaZair@users.noreply.github.com>
2023-04-29 00:40:07 -0400

    added server implementation plus running script implementation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MainPageComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { spawn } from 'child_process';
 
 @Controller()
 export class AppController {
@@ -7,6 +8,48 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Nest.js API is running!';
+  }
+
+  @Post('run')
+  async runScript(@Body('command') command: string): Promise<any> {
+    if (!command) {
+      throw new Error('No command provided');
+    }
+    const args = [
+      '-n',
+      '-p',
+      './assets/replays/00DA647E452FB993FF10598678FDBB67.replay',
+    ];
+
+    return new Promise((resolve, reject) => {
+      const childProcess = spawn(command, args);
+
+      let stdout = '';
+      let stderr = '';
+
+      childProcess.stdout.on('data', (data) => {
+        stdout += data;
+      });
+
+      childProcess.stderr.on('data', (data) => {
+        stderr += data;
+      });
+
+      childProcess.on('error', (error) => {
+        console.error(`Error executing command: ${error}`);
+        reject(`Error executing command: ${error}`);
+      });
+
+      childProcess.on('close', (code) => {
+        if (code !== 0) {
+          console.error(`Command exited with code ${code}`);
+          console.error(`stderr: ${stderr}`);
+          reject(`Error executing command: ${stderr}`);
+          return;
+        }
+        resolve(stdout);
+      });
+    });
   }
 }
