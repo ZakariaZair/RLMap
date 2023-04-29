89c0ebf8dde5e3d65eb0a9d699c2ccbb83a913a1
ZakariaZair <89856615+ZakariaZair@users.noreply.github.com>
2023-04-29 00:40:07 -0400

    added server implementation plus running script implementation

--- client/src/app/services/replay-loader-service/replay-loader.service.ts
+++ client/src/app/services/replay-loader-service/replay-loader.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReplayLoaderService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  loadReplay() {}
+
+  runScript() {
+    const commandPath = './command/rrrocket';
+    const command = commandPath;
+    this.http.post('http://localhost:3000/run', { command }).subscribe(
+      (response) => {
+        console.log('Script executed:', response);
+      },
+      (error) => {
+        console.error('Error executing script:', error);
+      }
+    );
+  }
 }
