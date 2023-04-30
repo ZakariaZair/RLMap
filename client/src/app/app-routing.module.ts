5fb1504f732a7c7e97df15d027d3e16e2d86cb16
ZakariaZair <89856615+ZakariaZair@users.noreply.github.com>
2023-04-30 14:42:10 -0400

    add new page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { ReplayPageComponent } from './pages/replay-page/replay-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: AppComponent },
   { path: 'map', component: MainPageComponent },
+  { path: 'replay', component: ReplayPageComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
