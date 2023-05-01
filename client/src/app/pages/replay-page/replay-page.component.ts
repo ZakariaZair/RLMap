12bc1f471cd7819a33655476739b42ee492bbcc7
ZakariaZair <89856615+ZakariaZair@users.noreply.github.com>
2023-04-30 22:47:24 -0400

    changed server

--- client/src/app/pages/replay-page/replay-page.component.ts
+++ client/src/app/pages/replay-page/replay-page.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReplayLoaderService } from 'src/app/services/replay-loader-service/replay-loader.service';
 
 @Component({
   selector: 'app-replay-page',
   templateUrl: './replay-page.component.html',
-  styleUrls: ['./replay-page.component.scss']
+  styleUrls: ['./replay-page.component.scss'],
 })
-export class ReplayPageComponent {
+export class ReplayPageComponent implements OnInit {
+  test: string = 'test';
 
+  constructor(private readonly replayLoaderService: ReplayLoaderService) {}
+
+  ngOnInit(): void {
+    this.test = this.replayLoaderService.getReplayData().network_frames.length;
+    console.log(this.test);
+  }
 }
