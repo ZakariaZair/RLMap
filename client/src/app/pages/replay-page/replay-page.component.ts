0708d919e77d18ab7b953f0c693516349657b606
ZakariaZair <89856615+ZakariaZair@users.noreply.github.com>
2023-05-03 19:05:49 -0400

    deploy

--- client/src/app/pages/replay-page/replay-page.component.ts
+++ client/src/app/pages/replay-page/replay-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ReplayLoaderService } from 'src/app/services/replay-loader-service/replay-loader.service';
 
 @Component({
@@ -6,17 +6,17 @@ import { ReplayLoaderService } from 'src/app/services/replay-loader-service/repl
   templateUrl: './replay-page.component.html',
   styleUrls: ['./replay-page.component.scss'],
 })
-export class ReplayPageComponent implements OnInit {
+export class ReplayPageComponent {
   test: number = 0;
 
   constructor(private readonly replayLoaderService: ReplayLoaderService) {}
 
-  ngOnInit(): void {
+  constructMap() {
     this.test =
       this.replayLoaderService.getReplayData().network_frames.frames[0].new_actors[1].initial_trajectory.location.x;
     console.log(
       this.replayLoaderService.getReplayData().network_frames.frames[0]
         .new_actors[1].initial_trajectory.location.x
     );
-  }
+  }  
 }
