c01b6339bd9bcadec56786e77cffa14ad3ffed1e
ZakariaZair <89856615+ZakariaZair@users.noreply.github.com>
2023-05-02 23:26:13 -0400

    Deploy

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ReplayPageComponent } from './pages/replay-page/replay-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: AppComponent },
   { path: 'home', component: MainPageComponent },
   { path: 'replay', component: ReplayPageComponent },
 ];
