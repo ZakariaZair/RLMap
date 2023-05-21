168259166dcff5ed2274a62a2067783feb23c0fe
ZakariaZair <zakaria.zair@polymtl.ca>
2023-05-21 11:06:56 -0400

    added new compo

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { ReplayPageComponent } from './pages/replay-page/replay-page.component';
+import { MapPageComponent } from './pages/map-page/map-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: MainPageComponent },
   { path: 'replay', component: ReplayPageComponent },
+  { path: 'map-editor', component: MapPageComponent },
 ];
 
 @NgModule({
