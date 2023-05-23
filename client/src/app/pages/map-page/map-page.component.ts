1b280688933cfc8eb349c4f51829a7632d3c7ef2
ZakariaZair <zakaria.zair@polymtl.ca>
2023-05-23 14:18:17 -0400

    build

--- client/src/app/pages/map-page/map-page.component.ts
+++ client/src/app/pages/map-page/map-page.component.ts
@@ -1,4 +1,11 @@
-import { AfterViewInit, Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { fabric } from 'fabric';
 
 @Component({
@@ -20,51 +27,50 @@ export class MapPageComponent implements AfterViewInit {
   }
 
   @HostListener('window:keydown', ['$event'])
-handleKeyDown(event: KeyboardEvent) {
-  const selectedObject = this.fabricCanvas.getActiveObject() as fabric.Image;
-  if (selectedObject) {
-    let position = selectedObject.getCenterPoint();
-    let currentAngle = selectedObject.angle || 0;
-    let angleChange = 0;
-    let positionChange = new fabric.Point(0, 0);
+  handleKeyDown(event: KeyboardEvent) {
+    const selectedObject = this.fabricCanvas.getActiveObject() as fabric.Image;
+    if (selectedObject) {
+      let position = selectedObject.getCenterPoint();
+      let currentAngle = selectedObject.angle || 0;
+      let angleChange = 0;
+      let positionChange = new fabric.Point(0, 0);
 
-    switch (event.key) {
-      case 'ArrowLeft':
-        angleChange = -22.5;
-        break;
-      case 'ArrowRight':
-        angleChange = 22.5;
-        break;
-      case 'ArrowUp':
-        currentAngle = 0;
-        break;
-      case 'ArrowDown':
-        currentAngle = 180;
-        break;
-      case 'a':
-        positionChange.x = -10; // Move left by 10 pixels
-        break;
-      case 'd':
-        positionChange.x = 10; // Move right by 10 pixels
-        break;
-      case 'w':
-        positionChange.y = -10; // Move up by 10 pixels
-        break;
-      case 's':
-        positionChange.y = 10; // Move down by 10 pixels
-        break;
+      switch (event.key) {
+        case 'ArrowLeft':
+          angleChange = -22.5;
+          break;
+        case 'ArrowRight':
+          angleChange = 22.5;
+          break;
+        case 'ArrowUp':
+          currentAngle = 0;
+          break;
+        case 'ArrowDown':
+          currentAngle = 180;
+          break;
+        case 'a':
+          positionChange.x = -10; // Move left by 10 pixels
+          break;
+        case 'd':
+          positionChange.x = 10; // Move right by 10 pixels
+          break;
+        case 'w':
+          positionChange.y = -10; // Move up by 10 pixels
+          break;
+        case 's':
+          positionChange.y = 10; // Move down by 10 pixels
+          break;
+      }
+
+      selectedObject.set({
+        angle: currentAngle + angleChange,
+        left: position.x + positionChange.x,
+        top: position.y + positionChange.y,
+      });
+
+      this.fabricCanvas.renderAll();
     }
-
-    selectedObject.set({
-      angle: currentAngle + angleChange,
-      left: position.x + positionChange.x,
-      top: position.y + positionChange.y,
-    });
-
-    this.fabricCanvas.renderAll();
   }
-}
-
 
   ngAfterViewInit() {
     this.setUpCanvas();
@@ -75,23 +81,39 @@ handleKeyDown(event: KeyboardEvent) {
 
   createMap() {
     const pathData = `
-    M ${this.fabricCanvas.width as number / 6} 10
-    L 10 ${this.fabricCanvas.height as number / 5}
-    L 10 ${(this.fabricCanvas.height as number * 2) / 5}
-    L 0 ${(this.fabricCanvas.height as number * 2) / 5}
-    L 0 ${(this.fabricCanvas.height as number * 3) / 5}
-    L 10 ${(this.fabricCanvas.height as number * 3) / 5}
-    L 10 ${(this.fabricCanvas.height as number * 4) / 5}
-    L ${this.fabricCanvas.width as number / 6} ${this.fabricCanvas.height as number - 10}
-    L ${(this.fabricCanvas.width as number * 5) / 6} ${this.fabricCanvas.height as number - 10}
-    L ${this.fabricCanvas.width as number - 10} ${(this.fabricCanvas.height as number * 4) / 5}
-    L ${this.fabricCanvas.width as number - 10} ${(this.fabricCanvas.height as number * 3) / 5}
-    L ${this.fabricCanvas.width as number} ${(this.fabricCanvas.height as number * 3) / 5}
-    L ${this.fabricCanvas.width as number} ${(this.fabricCanvas.height as number * 2) / 5}
-    L ${this.fabricCanvas.width as number - 10} ${(this.fabricCanvas.height as number * 2) / 5}
-    L ${this.fabricCanvas.width as number - 10} ${this.fabricCanvas.height as number / 5}
-    L ${(this.fabricCanvas.width as number * 5) / 6} 10
-    L ${this.fabricCanvas.width as number / 6} 10
+    M ${(this.fabricCanvas.width as number) / 6} 10
+    L 10 ${(this.fabricCanvas.height as number) / 5}
+    L 10 ${((this.fabricCanvas.height as number) * 2) / 5}
+    L 0 ${((this.fabricCanvas.height as number) * 2) / 5}
+    L 0 ${((this.fabricCanvas.height as number) * 3) / 5}
+    L 10 ${((this.fabricCanvas.height as number) * 3) / 5}
+    L 10 ${((this.fabricCanvas.height as number) * 4) / 5}
+    L ${(this.fabricCanvas.width as number) / 6} ${
+      (this.fabricCanvas.height as number) - 10
+    }
+    L ${((this.fabricCanvas.width as number) * 5) / 6} ${
+      (this.fabricCanvas.height as number) - 10
+    }
+    L ${(this.fabricCanvas.width as number) - 10} ${
+      ((this.fabricCanvas.height as number) * 4) / 5
+    }
+    L ${(this.fabricCanvas.width as number) - 10} ${
+      ((this.fabricCanvas.height as number) * 3) / 5
+    }
+    L ${this.fabricCanvas.width as number} ${
+      ((this.fabricCanvas.height as number) * 3) / 5
+    }
+    L ${this.fabricCanvas.width as number} ${
+      ((this.fabricCanvas.height as number) * 2) / 5
+    }
+    L ${(this.fabricCanvas.width as number) - 10} ${
+      ((this.fabricCanvas.height as number) * 2) / 5
+    }
+    L ${(this.fabricCanvas.width as number) - 10} ${
+      (this.fabricCanvas.height as number) / 5
+    }
+    L ${((this.fabricCanvas.width as number) * 5) / 6} 10
+    L ${(this.fabricCanvas.width as number) / 6} 10
     Z
   `;
     const path = new fabric.Path(pathData, {
@@ -117,16 +139,14 @@ handleKeyDown(event: KeyboardEvent) {
       '../../../assets/nobg_2.png',
       '../../../assets/nobg_2.png',
     ];
-  
-    const imagePromises = imageUrls.map((url) =>
-      this.loadImage(url)
-    );
-  
+
+    const imagePromises = imageUrls.map((url) => this.loadImage(url));
+
     Promise.all(imagePromises).then((images) => {
       images.forEach((img, index) => {
         img.set({
-          left: (this.fabricCanvas.width as number * (index + 1)) / 8,
-          top: this.fabricCanvas.height as number / 2,
+          left: ((this.fabricCanvas.width as number) * (index + 1)) / 8,
+          top: (this.fabricCanvas.height as number) / 2,
           originX: 'center',
           originY: 'center',
           hasRotatingPoint: true,
@@ -134,7 +154,7 @@ handleKeyDown(event: KeyboardEvent) {
           scaleX: 0.15,
           scaleY: 0.15,
         });
-  
+
         img.setControlsVisibility({
           bl: false,
           br: false,
@@ -146,15 +166,15 @@ handleKeyDown(event: KeyboardEvent) {
           tr: false,
           mtr: false,
         });
-  
+
         this.fabricCanvas.add(img);
         this.objects.push(img);
       });
-  
+
       this.fabricCanvas.renderAll();
     });
   }
-  
+
   loadImage(url: string): Promise<fabric.Image> {
     return new Promise((resolve) => {
       fabric.Image.fromURL(url, (img) => {
@@ -162,11 +182,12 @@ handleKeyDown(event: KeyboardEvent) {
       });
     });
   }
-  
 
   private setUpCanvas() {
     const canvasEl: HTMLCanvasElement = this.mapCanvas.nativeElement;
-    const parentDiv = canvasEl.parentElement ? canvasEl.parentElement : canvasEl;
+    const parentDiv = canvasEl.parentElement
+      ? canvasEl.parentElement
+      : canvasEl;
     canvasEl.width = parentDiv.offsetWidth;
     canvasEl.height = parentDiv.offsetHeight;
     this.mapWidth = canvasEl.width;
