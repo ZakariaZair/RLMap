fc1077a56672bac637de5647414b13e40b3922fb
ZakariaZair <zakaria.zair@polymtl.ca>
2023-06-24 18:21:37 -0400

    trying to fix map ctrl-z bug

--- client/src/app/pages/map-page/map-page.component.ts
+++ client/src/app/pages/map-page/map-page.component.ts
@@ -18,15 +18,20 @@ export class MapPageComponent implements AfterViewInit {
   drawingMode: boolean = false;
   gridMode: boolean = false;
   playersMode: boolean = false;
+  optionChosen: number;
   ballMode: boolean = false;
+  brushSize: number = 2;
+  brushColor: string = 'red';
   frames: number[];
+  private mapPath: string = '../../../assets/q6NlCWk01.svg';
   private fabricCanvas!: fabric.Canvas;
   private mapWidth: number;
   private mapHeight: number;
   private objects: Map<string, fabric.Image>;
   private mapBackground: fabric.Image;
-  private optionChosen: number;
-  private configurationIterator: number;
+  private configurationIndex: number;
+  private states: any[] = [];
+  private currentStateIndex: number = -1;
 
   constructor(private renderer: Renderer2) {
     this.mapWidth = 0;
@@ -35,11 +40,17 @@ export class MapPageComponent implements AfterViewInit {
     this.mapBackground = {} as fabric.Image;
     this.optionChosen = 3;
     this.frames = [0];
-    this.configurationIterator = 0;
+    this.configurationIndex = 0;
   }
 
   @HostListener('window:keydown', ['$event'])
   handleKeyDown(event: KeyboardEvent) {
+    const eventKey = event.key.toLowerCase();
+    if ((event.ctrlKey || event.metaKey) && eventKey === 'z') {
+      this.undo();
+      event.preventDefault();
+      return;
+    }
     this.setupInputs(event.key);
   }
 
@@ -47,36 +58,35 @@ export class MapPageComponent implements AfterViewInit {
     this.setUpCanvas();
     this.createMap();
     this.createObjects();
+    this.saveState();
   }
 
   createMap() {
-    fabric.loadSVGFromURL(
-      '../../../assets/q6NlCWk01.svg',
-      (objects, options) => {
-        const obj = fabric.util.groupSVGElements(objects, options);
-        obj.set({
-          opacity: 0.2,
-          left: -112,
-          top: 0,
-          fill: 'transparent',
-          stroke: 'black',
-          strokeWidth: 5,
-          selectable: false,
-          evented: false,
-          scaleX:
-            obj && obj.width
-              ? ((this.fabricCanvas.width as number) / obj.width) * 1.21
-              : 1,
-          scaleY:
-            obj && obj.height
-              ? ((this.fabricCanvas.height as number) / obj.height) * 0.98
-              : 1,
-        });
+    fabric.loadSVGFromURL(this.mapPath, (objects, options) => {
+      const obj = fabric.util.groupSVGElements(objects, options);
+      obj.set({
+        opacity: 0.2,
+        left: -112,
+        top: 0,
+        fill: 'transparent',
+        stroke: 'black',
+        strokeWidth: 5,
+        selectable: false,
+        evented: false,
+        scaleX:
+          obj && obj.width
+            ? ((this.fabricCanvas.width as number) / obj.width) * 1.21
+            : 1,
+        scaleY:
+          obj && obj.height
+            ? ((this.fabricCanvas.height as number) / obj.height) * 0.98
+            : 1,
+      });
+      const img = obj as fabric.Image;
 
-        this.fabricCanvas.add(obj).renderAll();
-        this.mapBackground = this.fabricCanvas.getObjects()[0] as fabric.Image;
-      }
-    );
+      this.fabricCanvas.add(img).renderAll();
+      this.mapBackground = img;
+    });
   }
 
   createObjects() {
@@ -103,7 +113,6 @@ export class MapPageComponent implements AfterViewInit {
           hasBorders: false,
           scaleX: img.getSrc().includes('ball') ? 0.15 : 0.1,
           scaleY: img.getSrc().includes('ball') ? 0.15 : 0.1,
-          type: img.getSrc().includes('ball') ? 'ball' : 'player',
         });
 
         img.on('mouseover', () => {
@@ -162,6 +171,7 @@ export class MapPageComponent implements AfterViewInit {
     this.objects.get('orange6')?.set('visible', false);
     this.optionChosen = 1;
     this.ensureObjChanges();
+    this.saveState();
   }
 
   option2() {
@@ -174,6 +184,7 @@ export class MapPageComponent implements AfterViewInit {
     this.objects.get('orange6')?.set('visible', false);
     this.optionChosen = 2;
     this.ensureObjChanges();
+    this.saveState();
   }
 
   option3() {
@@ -183,21 +194,23 @@ export class MapPageComponent implements AfterViewInit {
     });
     this.optionChosen = 3;
     this.ensureObjChanges();
+    this.saveState();
   }
 
   toggleBall() {
     this.objects.forEach((obj) => {
-      if (obj.get('type') === 'ball') {
+      if (obj.getSrc().includes('ball')) {
         obj.set('visible', !obj.get('visible'));
       }
     });
     this.ballMode = !this.ballMode;
     this.fabricCanvas.renderAll();
+    this.saveState();
   }
 
   togglePlayers() {
     this.objects.forEach((obj) => {
-      if (obj.get('type') === 'player') {
+      if (obj.getSrc().includes('blue') || obj.getSrc().includes('orange')) {
         obj.set('visible', !obj.get('visible'));
       }
     });
@@ -212,21 +225,132 @@ export class MapPageComponent implements AfterViewInit {
     }
     this.playersMode = !this.playersMode;
     this.fabricCanvas.renderAll();
+    this.saveState();
   }
 
   toggleDrawingMode() {
     this.drawingMode = !this.drawingMode;
     this.fabricCanvas.isDrawingMode = this.drawingMode;
+    this.saveState();
   }
 
   toggleGrid() {
     this.gridMode = !this.gridMode;
     this.mapBackground.set('visible', !this.gridMode);
     this.fabricCanvas.renderAll();
+    this.saveState();
   }
 
   addFrame() {
     this.frames.push(this.frames.length);
+    this.saveState();
+  }
+
+  changeBrushColor(color: string) {
+    this.fabricCanvas.freeDrawingBrush.color = color;
+    this.brushColor = color;
+    this.fabricCanvas.renderAll();
+  }
+
+  changeBrushWidth(width: number) {
+    this.fabricCanvas.freeDrawingBrush.width = width;
+    this.brushSize = width;
+    this.fabricCanvas.renderAll();
+    this.saveState();
+  }
+
+  undo() {
+    if (this.currentStateIndex > 0) {
+      this.currentStateIndex--;
+      this.fabricCanvas.loadFromJSON(
+        this.states[this.currentStateIndex],
+        () => {
+          this.fabricCanvas.getObjects().forEach((obj, index) => {
+            const img = obj as fabric.Image;
+            let key: string | null = null;
+            if (obj instanceof fabric.Image) {
+              if (img.getSrc().includes('ball')) {
+                key = 'ball';
+              } else if (img.getSrc().includes('blue')) {
+                key = 'blue' + (index - 1);
+              } else if (img.getSrc().includes('orange')) {
+                key = 'orange' + (index - 1);
+              }
+
+              console.log(key);
+
+              if (key) {
+                img.set({
+                  originX: 'center',
+                  originY: 'center',
+                  hasRotatingPoint: true,
+                  hasBorders: false,
+                  scaleX: img.getSrc().includes('ball') ? 0.15 : 0.1,
+                  scaleY: img.getSrc().includes('ball') ? 0.15 : 0.1,
+                });
+
+                img.on('mouseover', () => {
+                  img.set(
+                    'shadow',
+                    new fabric.Shadow({
+                      color: img.getSrc().includes('orange')
+                        ? 'orange'
+                        : img.getSrc().includes('ball')
+                        ? 'white'
+                        : 'blue',
+                      blur: 30,
+                      offsetX: 0,
+                      offsetY: 0,
+                    })
+                  );
+                });
+
+                img.on('mouseout', () => {
+                  img.set('shadow', undefined);
+                });
+
+                img.setControlsVisibility({
+                  bl: false,
+                  br: false,
+                  mb: false,
+                  ml: false,
+                  mr: false,
+                  mt: false,
+                  tl: false,
+                  tr: false,
+                  mtr: false,
+                });
+
+                
+
+                this.objects.set(key, img);
+              } 
+            } else if (obj.type === 'group') {
+              obj.set({
+                opacity: 0.2,
+                left: -112,
+                top: 0,
+                fill: 'transparent',
+                stroke: 'black',
+                strokeWidth: 5,
+                selectable: false,
+                evented: false
+              });
+              this.mapBackground = obj as fabric.Image;
+            }
+          });
+          this.fabricCanvas.renderAll();
+        }
+      );
+    }
+  }
+
+  private saveState() {
+    if (this.currentStateIndex + 1 < this.states.length) {
+      this.states = this.states.slice(0, this.currentStateIndex + 1);
+    }
+    this.states.push(this.fabricCanvas.toJSON());
+    this.currentStateIndex++;
   }
 
   private centerBall() {
@@ -237,7 +361,7 @@ export class MapPageComponent implements AfterViewInit {
     this.fabricCanvas.renderAll();
   }
 
-  private ensureObjChanges(){
+  private ensureObjChanges() {
     this.objects.forEach((obj) => {
       obj.setCoords();
     });
@@ -310,7 +434,15 @@ export class MapPageComponent implements AfterViewInit {
     this.fabricCanvas = new fabric.Canvas(canvasEl, {
       renderOnAddRemove: false,
     });
-    this.fabricCanvas.freeDrawingBrush.color = "red";
+    this.fabricCanvas.freeDrawingBrush.color = 'red';
     this.fabricCanvas.freeDrawingBrush.width = 5;
+
+    this.fabricCanvas.on('path:created', () => {
+      this.saveState();
+    });
+
+    this.fabricCanvas.on('object:modified', () => {
+      this.saveState();
+    });
   }
 }
