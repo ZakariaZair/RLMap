14f49bf7f056a96a037681d4b2779570b6123253
ZakariaZair <zakaria.zair@polymtl.ca>
2023-07-13 08:54:08 -0400

    console log

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -21,25 +21,17 @@ export class AppController {
 
     return new Promise((resolve, reject) => {
       let output = '';
-
       const childProcess = spawn(command, args);
-
       childProcess.stdout.on('data', (data) => {
-        console.log('Output:', data.toString());
         output += data.toString();
       });
-
       childProcess.on('error', (error) => {
-        console.error('Error occurred:', error);
         reject(error);
       });
-
       childProcess.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
         if (code !== 0) {
           reject(`Process exited with code ${code}`);
         } else {
-          console.log('Script resolved');
           resolve(output);
         }
       });
